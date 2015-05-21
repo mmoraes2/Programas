8d3040a2d82ff31af3f447dd0c05ead620687a03
Matheus Moraes <matheusmmoraes@yahoo.com.br>
2015-05-21 10:21:20 -0300

    Create Contador.c

---
+++ Contador.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	int num;
+	int soma;
+	int contador_final = 1000;
+
+	soma = 0;
+	while()
+	{
+		
+		soma = soma + 1;
+	}
+
+
+	getchar();
+	return 0;
+}
